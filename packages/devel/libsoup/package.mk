3452d21c62008e6a67904a80b8924a040b6540ca
Oren Sokoler <orenskl@gmail.com>
2023-01-20 08:39:50 +0200

    Fixed dependency for clean build

--- packages/devel/libsoup/package.mk
+++ packages/devel/libsoup/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="2e7a1313c2223c7e70cda90a8c02c679ab2bf3935c07b5bb028a9b5550411b96"
 PKG_LICENSE="GPL-2.1-or-later"
 PKG_SITE="https://libsoup.org/libsoup-3.0/index.html"
 PKG_URL="https://gitlab.gnome.org/GNOME/libsoup/-/archive/${PKG_VERSION}/libsoup-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libpsl glib-networking"
+PKG_DEPENDS_TARGET="toolchain libpsl glib-networking libxml2"
 PKG_LONGDESC="HTTP client/server library for GNOME"
 
 pre_configure_target() {
