29c55c78d75ea6383d179029b803d8161f5eac66
Oren Sokoler <orenskl@gmail.com>
2023-01-28 19:36:18 +0200

    More dependency fixes

--- packages/graphics/libwpe/package.mk
+++ packages/graphics/libwpe/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="c073305bbac5f4402cc1c8a4753bfa3d63a408901f86182051eaa5a75dd89c00"
 PKG_LICENSE="GPL"
 PKG_SITE="https://wpewebkit.org"
 PKG_URL="https://wpewebkit.org/releases/libwpe-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain mesa"
 PKG_LONGDESC="The libwpe package contains a general purpose library for WPE WebKit and the WPE Renderer."
 
 PKG_MESON_OPTS_TARGET="-Denable-xkb=false"
