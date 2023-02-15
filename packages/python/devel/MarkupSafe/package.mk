da82d47f3d105ae078b09c4110b0a9ea43015f57
Oren Sokoler <orenskl@gmail.com>
2023-02-15 16:29:25 +0200

    Added Jinja2 and MarkupSafe (#20)

--- packages/python/devel/MarkupSafe/package.mk
+++ packages/python/devel/MarkupSafe/package.mk
@@ -3,15 +3,28 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="MarkupSafe"
-PKG_VERSION="1.1.1"
-PKG_SHA256="29872e92839765e546828bb7754a68c418d927cd064fd4708fab9fe9c8bb116b"
+PKG_VERSION="2.1.1"
+PKG_SHA256="7f91197cc9e48f989d12e4e6fbc46495c446636dfc81b9ccf50bb0ec74b91d4b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/MarkupSafe/"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="Python3:host setuptools:host"
+PKG_DEPENDS_TARGET="toolchain Python3"
 PKG_LONGDESC="MarkupSafe implements a XML/HTML/XHTML Markup safe string for Python"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_host() {
   exec_thread_safe python3 setup.py install --prefix=${TOOLCHAIN}
+}
+
+make_target() {
+  python3 setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python3 setup.py install --root=${INSTALL} --prefix=/usr
+}
+
+post_makeinstall_target() {
+  python_remove_source
 }
