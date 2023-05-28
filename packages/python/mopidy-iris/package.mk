1ba52cac5299d2d1d5ff7234fb665953bac63716
Oren Sokoler <orenskl@gmail.com>
2023-05-28 16:10:24 +0300

    Updated mopidy-iris and tidalapi (#41)

--- packages/python/mopidy-iris/package.mk
+++ packages/python/mopidy-iris/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2023 Oren Sokoler
 
 PKG_NAME="mopidy-iris"
-PKG_VERSION="3.66.0"
-PKG_SHA256="f26fac05d7b038a79ea8acf6d52f096e0519edb089d08eb5a7374c8158e8ded8"
+PKG_VERSION="3.67.0"
+PKG_SHA256="b06e39ea4f80d84e0f62f632f01c8aaec8842c75095f5ea5c066138a70f4de4e"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://pypi.org/project/Mopidy-Iris/"
-PKG_URL="https://files.pythonhosted.org/packages/30/e8/051d69c47140a1c055115e0bd9d7c6b90cc9ed2bc997a49e673d106bb3e1/Mopidy-Iris-${PKG_VERSION}.tar.gz"
+PKG_URL="https://files.pythonhosted.org/packages/4d/72/6b2f0ff046624e568ae0e21b8e5b63e169400d4efdc2b10984820e514bf1/Mopidy-Iris-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="Python3 setuptools mopidy"
 PKG_LONGDESC="Discover, explore and manage your music library across multiple sources with this beautiful web-based interface."
 PKG_TOOLCHAIN="manual"
--- packages/python/tidalapi/package.mk
+++ packages/python/tidalapi/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023 Oren Sokoler
 
 PKG_NAME="tidalapi"
-PKG_VERSION="0.7.0"
-PKG_SHA256="287a7f61df4b127eb68432af7f5a62185144f6c2116393fb65df48c3b5b9157b"
+PKG_VERSION="0.7.1"
+PKG_SHA256="7ce1a8e89253f3580ef4a8d48206580da233780e602c02b52f5acb6b06c9201e"
 PKG_LICENSE="LGPLv3"
 PKG_SITE="https://pypi.org/project/tidalapi/"
 PKG_URL="https://github.com/tamland/python-tidal/archive/refs/tags/v${PKG_VERSION}.tar.gz"
